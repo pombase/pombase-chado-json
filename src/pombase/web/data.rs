60fc26227e5b371eff1fd469890954d418061945
Kim Rutherford <kmr44@cam.ac.uk>
2016-10-31 13:18:20 +1300

    Move pombase::{db,web} to their own files

---
+++ src/pombase/web/data.rs
@@ -0,0 +1,109 @@
+use std::collections::HashMap;
+
+pub type GeneUniquename = String;
+pub type GeneName = String;
+pub type TypeName = String;
+
+#[derive(Serialize, Clone)]
+pub struct GeneShort {
+    pub uniquename: GeneUniquename,
+    pub name: Option<GeneName>,
+}
+
+pub type TranscriptUniquename = String;
+
+#[derive(Serialize)]
+pub struct TranscriptShort {
+    pub uniquename: TranscriptUniquename,
+    //                pub exons: Vec<ExonShort>,
+    //                pub utrs: Vec<UTRShort>,
+}
+
+pub type TermName = String;
+pub type TermId = String;
+pub type TermDef = String;
+
+#[derive(Serialize)]
+pub struct TermShort {
+    pub name: TermName,
+    pub termid: TermId,
+    pub is_obsolete: bool,
+}
+
+#[derive(Serialize, Clone)]
+pub struct PublicationShort {
+    pub uniquename: String,
+    pub title: Option<String>,
+    pub citation: Option<String>,
+}
+
+type Evidence = String;
+
+#[derive(Serialize)]
+pub struct FeatureAnnotation {
+    pub term: TermShort,
+    pub evidence: Option<Evidence>,
+    pub publication: Option<PublicationShort>,
+}
+
+pub type TypeFeatureAnnotationMap =
+    HashMap<TypeName, Vec<FeatureAnnotation>>;
+
+#[derive(Serialize)]
+pub struct GeneDetails {
+    pub uniquename: GeneUniquename,
+    pub name: Option<String>,
+    pub transcripts: Vec<TranscriptShort>,
+    pub annotations: TypeFeatureAnnotationMap,
+}
+
+pub type UniquenameGeneMap =
+    HashMap<GeneUniquename, GeneDetails>;
+
+#[derive(Serialize)]
+pub struct TranscriptDetails {
+    pub uniquename: TranscriptUniquename,
+    pub name: Option<String>,
+    pub annotations: TypeFeatureAnnotationMap,
+}
+
+pub type UniquenameTranscriptMap =
+    HashMap<TranscriptUniquename, TranscriptDetails>;
+
+#[derive(Serialize, Clone)]
+pub struct TermAnnotation {
+    pub gene: GeneShort,
+    pub evidence: Option<Evidence>,
+    pub publication: Option<PublicationShort>,
+}
+
+#[derive(Serialize)]
+pub struct TermDetails {
+    pub name: TermName,
+    pub termid: TermId,
+    pub definition: Option<TermDef>,
+    pub is_obsolete: bool,
+    pub annotations: Vec<TermAnnotation>,
+}
+
+impl Clone for TermDetails {
+    fn clone(&self) -> TermDetails {
+        TermDetails {
+            name: self.name.clone(),
+            termid: self.termid.clone(),
+            definition: self.definition.clone(),
+            is_obsolete: self.is_obsolete,
+            annotations: self.annotations.clone(),
+        }
+    }
+}
+
+pub type IdGeneMap = HashMap<GeneUniquename, GeneDetails>;
+pub type IdTermMap = HashMap<TermId, TermDetails>;
+
+#[derive(Serialize)]
+pub struct Metadata {
+    pub db_creation_datetime: String,
+    pub gene_count: usize,
+    pub term_count: usize,
+}
