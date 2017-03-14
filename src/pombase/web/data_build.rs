bec210a0b0e7d34300a50af7c0544efe67e439d4
Kim Rutherford <kmr44@cam.ac.uk>
2017-03-15 00:43:09 +1300

    Fix missing phenotypes on term and ref. pages
    Refs pombase/website#209

--- src/pombase/web/data_build.rs
+++ src/pombase/web/data_build.rs
@@ -222,10 +222,8 @@ pub fn remove_redundant_summary_rows(rows: &mut Vec<TermSummaryRow>) {
             panic!("remove_redundant_summary_rows() failed: num genes > 1\n");
         }
 
-        if prev.gene_uniquenames.len() == 0 && current.gene_uniquenames.len() == 0 &&
-            (prev.genotype_uniquename.is_none() && current.genotype_uniquename.is_none() ||
-             prev.genotype_uniquename.unwrap() == current.genotype_uniquename.clone().unwrap()) ||
-            prev.gene_uniquenames == current.gene_uniquenames {
+        if (&prev.gene_uniquenames, &prev.genotype_uniquename) ==
+            (&current.gene_uniquenames, &current.genotype_uniquename) {
                 if !vec_set.contains_superset(&current.extension) {
                     results.push(current.clone());
                     vec_set.insert(&current.extension);
