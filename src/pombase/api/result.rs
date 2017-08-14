9d16a5403e3928a3308e87c81a6b1bedc89755af
Kim Rutherford <kmr44@cam.ac.uk>
2017-08-14 16:01:44 +1200

    Omit the sequence field from output if empty
    Refs pombase/website#21

--- src/pombase/api/result.rs
+++ src/pombase/api/result.rs
@@ -3,6 +3,7 @@ use api::query::Query;
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ResultRow {
     pub gene_uniquename: String,
+    #[serde(skip_serializing_if="Option::is_none")]
     pub sequence: Option<String>,
 }
 
