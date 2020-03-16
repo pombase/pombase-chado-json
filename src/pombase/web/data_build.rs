8d5547af06b8ca28ae17d726b7332de472555636
Kim Rutherford <kmr@aska.gen.nz>
2020-03-17 00:27:00 +1300

    Truncate molecular weight to 2 DP in query results

--- src/pombase/web/data_build.rs
+++ src/pombase/web/data_build.rs
@@ -3982,7 +3982,7 @@ impl <'a> WebDataBuild<'a> {
 
         for transcript in &gene_details.transcripts {
             if let Some(ref protein) = transcript.protein {
-                molecular_weight = Some(protein.molecular_weight);
+                molecular_weight = Some((100.0 * protein.molecular_weight).round() / 100.0);
                 if protein.sequence.ends_with("*") {
                     protein_length = Some(protein.sequence.len() - 1);
                 } else {
