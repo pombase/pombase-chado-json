pombase/pombase-chado-json

commit c15efc67180bb7389794e5168aa627858a864ea3

Fix to use index on JSON column in query table

src/pombase/api/site_db.rs
@@ -43,21 +43,29 @@ impl SiteDB {
     }
 
     pub fn id_from_query(&self, query: &Query) -> Option<Uuid> {
-        let query_value = match serde_json::value::to_value(query) {
-            Ok(v) => v,
-            Err(_) => return None,
+        let query_value: String = match serde_json::value::to_value(query) {
+            Ok(v) => v.to_string(),
+            Err(err) => {
+                eprint!("error converting query to string: {:?}\n", err);
+                return None;
+            }
         };
 
-        let rs = match self.conn.query("SELECT id FROM query WHERE query_json = $1",
+        let rs = match self.conn.query("SELECT id::uuid FROM query WHERE digest(query_json, 'sha256') = digest($1, 'sha256');",
                                        &[&query_value])
         {
             Ok(rs) => rs,
-            Err(_) => return None,
+            Err(err) => {
+                eprint!("error querying in id_from_query(): {:?}\n", err);
+                return None;
+            }
         };
 
         if rs.len() > 0 {
-            Some(rs.get(0).get(0))
+            let id: Uuid = rs.get(0).get("id");
+            Some(id)
         } else {
+            eprint!("query not in DB: {}\n", query_value);
             None
         }
     }
@@ -68,8 +76,8 @@ impl SiteDB {
             Err(e) => return Err(format!("couldn't begin transaction: {}", e)),
         };
 
-        let serde_value = match serde_json::value::to_value(&query) {
-            Ok(v) => v,
+        let serde_value: String = match serde_json::value::to_value(&query) {
+            Ok(v) => v.to_string(),
             Err(e) => return Err(format!("serde error: {}", e)),
         };
 

commit 1754a65188e8d604618b51482074e5a0056af4c6

Fix internal error in query_by_id()
Broken by c15efc67180bb7389

Rocket.toml
@@ -7,10 +7,10 @@ log = "debug"
 [staging]
 address = "0.0.0.0"
 port = 8500
-log = "debug"
+log = "normal"
 workers = 8
 
 [production]
 address = "0.0.0.0"
 port = 8500
-log = "debug"
+log = "normal"

src/pombase/api/site_db.rs
@@ -29,9 +29,9 @@ impl SiteDB {
         };
 
         if rs.len() > 0 {
-            let query_value: Option<serde_json::Value> = rs.get(0).get(0);
+            let query_value: Option<String> = rs.get(0).get(0);
 
-            let query: Query = match serde_json::value::from_value(query_value.unwrap()) {
+            let query: Query = match serde_json::from_str(&query_value.unwrap()) {
                 Ok(v) => v,
                 Err(_) => return None,
             };