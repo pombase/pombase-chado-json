b2143607a2bc897e85cc4147b1b32df701b6b7d3
Kim Rutherford <kmr@aska.gen.nz>
2020-11-15 19:54:06 +1300

    Remove ND lines for ncRNA genes with no annotation
    Refs pombase/pombase-chado#801

--- src/pombase/bio/go_format_writer.rs
+++ src/pombase/bio/go_format_writer.rs
@@ -108,6 +108,22 @@ pub fn write_gene_product_annotation(gpad_writer: &mut dyn io::Write,
     let local_iso_date = local.format("%F");
     let assigned_by = &config.database_name;
 
+    if gene_details.feature_type == "ncRNA gene" {
+        let mut found = false;
+        for aspect in GO_ASPECT_NAMES.iter() {
+            let term_annotations = gene_details.cv_annotations.get(&RcString::from(aspect));
+            if term_annotations.is_some() {
+                found = true;
+                break;
+            }
+        }
+        if !found {
+            // special case for ncRNA genes: only write ND lines for an aspect
+            // if there are some annotations for the other aspects
+            return Ok(());
+        }
+    }
+
     for aspect in GO_ASPECT_NAMES.iter() {
         let term_annotations = gene_details.cv_annotations.get(&RcString::from(aspect));
         if term_annotations.is_none() {
