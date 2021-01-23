pombase/pombase-chado-json

commit 049bdb768b8751d0cf11b0ae8ed8836f62731e52

Change to new gene expression namespaces
Refs pombase/pombase-chado#802

src/pombase/web/data_build.rs
@@ -3306,7 +3306,7 @@ impl <'a> WebDataBuild<'a> {
         let cv_name = term_details.cv_name.clone();
 
         match cv_name.as_ref() {
-            "gene_ex" => {
+            "PomGeneExRNA" | "PomGeneExProt" => {
                 if is_not {
                     panic!("gene_ex annotations can't be NOT annotations");
                 }

commit 06097fe822c234f43887029b2a3c6bac733503fb

Fix missing quantitative gene expression
The section was missing from the gene pages.

Broken by 049bdb768b8751d0

Refs pombase/pombase-chado#802
Refs pombase/website#1637

src/pombase/web/data_build.rs
@@ -3306,7 +3306,7 @@ impl <'a> WebDataBuild<'a> {
         let cv_name = term_details.cv_name.clone();
 
         match cv_name.as_ref() {
-            "PomGeneExRNA" | "PomGeneExProt" => {
+            "gene_ex" | "PomGeneExRNA" | "PomGeneExProt" => {
                 if is_not {
                     panic!("gene_ex annotations can't be NOT annotations");
                 }