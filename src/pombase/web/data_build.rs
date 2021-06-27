pombase/pombase-chado-json

commit e5a85e381046a73df4e8902c033d6a0a8b14cd6a

Allow ext. links including gene IDs in annotation
It's now possible to add an external link from an annotation and include
the gene uniquename in the URL.

Refs pombase/website#1239

src/pombase/web/data_build.rs
@@ -2825,10 +2825,19 @@ impl <'a> WebDataBuild<'a> {
 
                 for (source_name, source_config) in cv_config.source_config {
                     let mut maybe_xref_id = None;
-                    if let Some(ref term_xref_id_prop) = source_config.id_prop {
-                        for cvtermprop in cvterm.cvtermprops.borrow().iter() {
-                            if cvtermprop.prop_type.name == *term_xref_id_prop {
-                                maybe_xref_id = Some(cvtermprop.value.clone());
+                    if let Some(ref term_xref_id_prop) = source_config.id_source {
+                        if term_xref_id_prop.starts_with("prop_name:") {
+                            let term_xref_id_prop = &term_xref_id_prop[9..];
+                            for cvtermprop in cvterm.cvtermprops.borrow().iter() {
+                                if cvtermprop.prop_type.name == *term_xref_id_prop {
+                                    maybe_xref_id = Some(cvtermprop.value.clone());
+                                    break;
+                                }
+                            }
+                        } else {
+                            if term_xref_id_prop == "ACCESSION" {
+                                let dbxref: &Dbxref = cvterm.dbxref.borrow();
+                                maybe_xref_id = Some(dbxref.accession.clone());
                             }
                         }
                     }

src/pombase/web/config.rs
@@ -76,7 +76,8 @@ pub struct CvSourceConfig {
     // a type name for the cvtermprop to display to the user
     pub display_name_prop: Option<RcString>,
     // the cvtermprop type name for the ID used for linking
-    pub id_prop: Option<RcString>,
+    // or "ACCESSION" if the accession ID of the term should be used
+    pub id_source: Option<RcString>,
 }
 
 pub type TargetRelationName = String;

commit 27ea2c10711b652ee3b2bd67448572e5e1df8ce7

Fix missing MalaCards links
Broken by e5a85e381046

src/pombase/web/data_build.rs
@@ -2827,7 +2827,7 @@ impl <'a> WebDataBuild<'a> {
                     let mut maybe_xref_id = None;
                     if let Some(ref term_xref_id_prop) = source_config.id_source {
                         if term_xref_id_prop.starts_with("prop_name:") {
-                            let term_xref_id_prop = &term_xref_id_prop[9..];
+                            let term_xref_id_prop = &term_xref_id_prop[10..];
                             for cvtermprop in cvterm.cvtermprops.borrow().iter() {
                                 if cvtermprop.prop_type.name == *term_xref_id_prop {
                                     maybe_xref_id = Some(cvtermprop.value.clone());