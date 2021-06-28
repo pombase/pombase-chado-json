pombase/pombase-chado-json

commit 673ad261867dd04e83403873770d778cae008850

Allow ID+name as an option for with and from
Refs japonicusdb/japonicus-config#20

src/pombase/web/data_build.rs
@@ -520,6 +520,8 @@ fn cmp_gene_vec(genes: &UniquenameGeneMap,
 
 lazy_static! {
     static ref PROMOTER_RE: Regex = Regex::new(r"^(?P<gene>.*)-promoter$").unwrap();
+    static ref PREFIX_AND_ID_RE: Regex =
+        Regex::new(r"^(?P<prefix>\S+):(?P<id>\S+)$").unwrap();
 }
 
 pub fn cmp_ont_annotation_detail(cv_config: &CvConfig,
@@ -3168,20 +3170,35 @@ impl <'a> WebDataBuild<'a> {
         }
     }
 
+
     fn make_with_or_from_value(&self, with_or_from_value: &RcString) -> WithFromValue {
-        let db_prefix_patt = format!("^{}:", self.config.database_name);
-        let re = Regex::new(&db_prefix_patt).unwrap();
-        let gene_uniquename = RcString::from(re.replace_all(&with_or_from_value, "").as_ref());
-        if self.genes.contains_key(&gene_uniquename) {
-            let gene_short = self.make_gene_short(&gene_uniquename);
-            WithFromValue::Gene(gene_short)
-        } else {
-            if self.terms.get(with_or_from_value).is_some() {
-                WithFromValue::Term(self.make_term_short(with_or_from_value))
-            } else {
-                WithFromValue::Identifier(with_or_from_value.clone())
+        if let Some(captures) = PREFIX_AND_ID_RE.captures(with_or_from_value) {
+            let prefix = &captures["prefix"];
+            let id = &captures["id"];
+
+            if self.genes.contains_key(id) {
+                let gene_short = self.make_gene_short(id);
+                if self.config.database_name == prefix {
+                    // a gene from the main organism
+                    return WithFromValue::Gene(gene_short);
+                } else {
+                    if let Some(name) = &gene_short.name {
+                        return WithFromValue::IdentifierAndName({
+                            IdentifierAndName {
+                                identifier: with_or_from_value.clone(),
+                                name: RcString::from(name),
+                            }
+                        });
+                    }
+                }
             }
         }
+
+        if self.terms.get(with_or_from_value).is_some() {
+            return WithFromValue::Term(self.make_term_short(with_or_from_value))
+        }
+
+        WithFromValue::Identifier(with_or_from_value.clone())
     }
 
     // process annotation

src/pombase/data_types.rs
@@ -495,13 +495,20 @@ impl OrthologAnnotationContainer for ReferenceDetails {
     }
 }
 
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, Hash)]
+pub struct IdentifierAndName {
+    pub identifier: RcString,
+    pub name: RcString,
+}
+
 // the GO with/from
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, Hash)]
 #[serde(rename_all = "snake_case")]
 pub enum WithFromValue {
     Gene(GeneShort),
     Term(TermShort),
-    Identifier(RcString)
+    Identifier(RcString),
+    IdentifierAndName(IdentifierAndName),
 }
 
 impl From<WithFromValue> for RcString {
@@ -510,6 +517,9 @@ impl From<WithFromValue> for RcString {
             WithFromValue::Gene(gene) => gene.uniquename,
             WithFromValue::Term(term) => term.termid,
             WithFromValue::Identifier(id) => id,
+            WithFromValue::IdentifierAndName(id_and_name) =>
+                RcString::from(&format!("{} ({})", id_and_name.name,
+                                        id_and_name.identifier)),
         }
     }
 }
@@ -521,12 +531,14 @@ impl Ord for WithFromValue {
                 WithFromValue::Gene(ref gene) => &gene.uniquename,
                 WithFromValue::Term(ref term) => &term.name,
                 WithFromValue::Identifier(ref id) => id,
+                WithFromValue::IdentifierAndName(ref id_and_name) => &id_and_name.identifier,
             };
         let other_string =
             match other {
                 WithFromValue::Gene(ref gene) => &gene.uniquename,
                 WithFromValue::Term(ref term) => &term.name,
                 WithFromValue::Identifier(ref id) => id,
+                WithFromValue::IdentifierAndName(ref id_and_name) => &id_and_name.identifier,
             };
 
         self_string.cmp(other_string)

commit 6aa5bc7cba8f84f2ca059169c99dc06b22a54ec3

Fix missing genes in with field
Bug caused by 673ad261867dd04e8340387

Refs japonicusdb/japonicus-config#20

src/pombase/web/data_build.rs
@@ -3192,6 +3192,12 @@ impl <'a> WebDataBuild<'a> {
                     }
                 }
             }
+        } else {
+            if self.genes.contains_key(with_or_from_value) {
+                let gene_short = self.make_gene_short(with_or_from_value);
+                // a gene from the main organism
+                return WithFromValue::Gene(gene_short);
+            }
         }
 
         if self.terms.get(with_or_from_value).is_some() {