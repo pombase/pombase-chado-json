pombase/pombase-chado-json

commit e450f5f669926b93a73733d8bc33a9678ad47928

In completion return only terms with annotations
Unless a term ID is the search string.

Refs pombase/website#726

src/pombase/api/term_search.rs
@@ -112,7 +112,7 @@ pub fn make_terms_url(config: &ServerConfig, cv_name: &str, q: &str) -> Option<S
             return None;
         }
 
-        terms_url += " AND (name:(";
+        terms_url += "annotation_count:[1 TO *] AND (name:(";
 
         let query_part = get_query_part(&clean_words);
 

src/pombase/data_types.rs
@@ -400,6 +400,20 @@ pub trait AnnotationContainer: Container {
     fn terms_by_termid(&self) -> &TermShortOptionMap;
     fn genes_by_uniquename(&self) -> &GeneShortOptionMap;
     fn genotypes_by_uniquename(&self) -> Option<&HashMap<GenotypeUniquename, GenotypeShort>>;
+
+    fn annotation_count(&self) -> usize {
+        let mut annotation_ids = HashSet::new();
+
+        for term_annotations in self.cv_annotations().values() {
+            for term_annotation in term_annotations {
+                for annotation_detail_id in &term_annotation.annotations {
+                    annotation_ids.insert(annotation_detail_id);
+                }
+            }
+        }
+
+        annotation_ids.len()
+    }
 }
 
 pub trait OrthologAnnotationContainer: AnnotationContainer {
@@ -1572,6 +1586,10 @@ pub struct SolrTermSummary {
     pub interesting_parent_ids: HashSet<RcString>,
     #[serde(skip_serializing_if="HashSet::is_empty", default)]
     pub secondary_identifiers: HashSet<TermId>,
+
+    pub annotation_count: usize,
+    pub gene_count: usize,
+    pub genotype_count: usize,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]

src/pombase/web/data_build.rs
@@ -5406,6 +5406,7 @@ impl <'a> WebDataBuild<'a> {
                 })
                 .collect::<Vec<_>>();
 
+            let annotation_count = term_details.annotation_count();
             let interesting_parent_ids_for_solr =
                 term_details.interesting_parent_ids.clone();
             let term_summ = SolrTermSummary {
@@ -5419,6 +5420,10 @@ impl <'a> WebDataBuild<'a> {
                 distant_synonym_words: RcString::from(&distant_synonym_words_vec.join(" ")),
                 interesting_parent_ids: interesting_parent_ids_for_solr,
                 secondary_identifiers: term_details.secondary_identifiers.clone(),
+                annotation_count,
+                gene_count: term_details.gene_count,
+                genotype_count: term_details.genotype_count,
+
             };
             return_summaries.push(term_summ);
         }

commit 3860ac6aff2bda76f4dbbcd8b5b84a36e59f1297

Fix mangled URL for Solr term search
Broken by e450f5f66992

Refs pombase/website#1722

src/pombase/api/term_search.rs
@@ -112,7 +112,7 @@ pub fn make_terms_url(config: &ServerConfig, cv_name: &str, q: &str) -> Option<S
             return None;
         }
 
-        terms_url += "annotation_count:[1 TO *] AND (name:(";
+        terms_url += " AND annotation_count:[1 TO *] AND (name:(";
 
         let query_part = get_query_part(&clean_words);
 