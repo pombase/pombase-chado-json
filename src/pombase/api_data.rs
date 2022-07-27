3402c446bd1ca4620f7c2e86ad8d3611fad92837
Kim Rutherford <kmr@aska.gen.nz>
2022-07-27 20:24:34 +1200

    Merge exts. when PR ID has the 'has_input' rel.
    Refs pombase/website#1850
    Refs pombase/website#1710
    Refs pombase/pombase-chado#974

--- src/pombase/api_data.rs
+++ src/pombase/api_data.rs
@@ -388,7 +388,7 @@ impl APIData {
                 // drop the with for this annotation
                 if let Some(with_value) = first_with.clone() {
                     annotation.extension.iter_mut().for_each(|mut ext_part| {
-                        if ext_part.rel_type_name == "has_direct_input" {
+                        if ext_part.rel_type_name == "has_input" {
                             match &mut ext_part.ext_range {
                                 ExtRange::GeneProduct(range_termid) => {
                                     if let WithFromValue::Gene(gene_short) = &with_value {
