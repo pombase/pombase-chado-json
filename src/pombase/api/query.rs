cb2e7eb5e1bff017995e90c46736889f51d2e8b2
Kim Rutherford <kmr@aska.gen.nz>
2025-03-07 21:27:42 +1300

    Add querying by GO-CAM ID to server code
    Refs pombase/website#2346

--- src/pombase/api/query.rs
+++ src/pombase/api/query.rs
@@ -365,7 +365,16 @@ fn exec_ref_genes(api_data: &APIData, reference_uniquename: &FlexStr) -> GeneUni
 }
 
 fn exec_subset(api_data: &APIData, subset_name: &FlexStr)  -> GeneUniquenameVecResult {
-    Ok(api_data.genes_of_subset(subset_name))
+    if subset_name.starts_with("gomodel:") {
+        let gocam_id = subset_name.strip_prefix("gomodel:").unwrap();
+        if let Some(gocam_details) = api_data.get_gocam_details_by_id(gocam_id) {
+            Ok(gocam_details.genes.iter().cloned().collect())
+        } else {
+            Ok(vec![])
+        }
+    } else {
+        Ok(api_data.genes_of_subset(subset_name))
+    }
 }
 
 fn exec_gene_list(genes: &[GeneShort])
